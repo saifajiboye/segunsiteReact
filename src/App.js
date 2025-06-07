01cafd19771970cc757f7c100532deadef7316ef
saifajiboye <83846088+saifajiboye@users.noreply.github.com>
2025-06-07 14:04:01 -0600

    Nav bar setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import logo from './app/assets/img/saifullah.png';
 
 function App() {
   return (
     <div className="App">
-      hello nucamp!
+      <Navbar dark color='primary' sticky='top' expand='md'>
+               <Container>
+                    <NavbarBrand href='/'>
+                        <img src={logo} alt='nucamp logo' />
+                    </NavbarBrand>
+                </Container>
+            </Navbar>
+      Hello! From Segun
     </div>
   );
 }
